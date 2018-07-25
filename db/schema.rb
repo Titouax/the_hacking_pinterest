37d3dc65680b3f4118c46b03a232d5a55935ae37
Titouax <titouan.dessus@gmail.com>
2018-07-25 23:23:43 +0200

    ajout du seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_205018) do
+ActiveRecord::Schema.define(version: 2018_07_25_211312) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "pin_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
     t.index ["pin_id"], name: "index_comments_on_pin_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |index|
+  users = User.create(name: "Name#{index}")
+end
+
+10.times do |index|
+  pins = Pin.create(url: "www.lien-image-#{index}.co", users_id: "#{index}".to_i)
+end
+
+10.times do |index|
+  comments = Comment.create(pin_id: "#{index}".to_i, body: "Voici un commentaire" , user_id: "#{index}".to_i)
+end
